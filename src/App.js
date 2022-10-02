bbf36e695acf32fd97044eca56edf069ba32bcdd
Well-appedidos <wellington.lira@appedidos.com.br>
2022-10-01 23:14:06 -0300

    0.0.1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      	Meu nome é: ... e tenho ... anos.<br/>
+		Tema: ...
+
+		<hr/>
+		<input text="text" value={'...'} />
+
+		<hr/>
+
+		<button>Switch Theme</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
